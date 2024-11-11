33a11fdbf84473d159346ca9bc9353ef45d97afc
MatisBenitez <matiasbenitezdavid@gmail.com>
2024-11-11 11:22:55 +0100

    segundo puesh

--- curso_tutorizado_python/estructurasBasicas.py
+++ curso_tutorizado_python/estructurasBasicas.py
@@ -1,10 +1,28 @@
 #First lesson, basic type of operators and variables
-    
+
+#Python is object oriented so each var is an object
+
 # There are different types of numbers: integers, float numbers and complex number 
 #There are String types
 #logic types: booleans(true or false), & !=
 #special operators (is,in,not in, is not)
+
 variable="Im a variable"
+print(type(variable))#to know what type is the var
+
+#A variable is space of the memory of the computer with value that could change in the program thats why the name
+
+operadores="These are the results of operations: "
+print(10%3,10**2,9//2)
+#modulo, potency and natural split
+#the type is defined by the content of the same so its not like java
+
+menssage="""Aqui hay algunos
+saltos 
+de linea"""
+print(menssage)
+
+
 print(variable)
 a=0
 for i in range (100):
