85ec6d4aa4bf432d43c1cc5f24e431fb5a3d1e9c
MatisBenitez <matiasbenitezdavid@gmail.com>
2024-11-18 16:38:34 +0100

    nuevo commit

--- curso_tutorizado_python/condicionales.py
+++ curso_tutorizado_python/condicionales.py
@@ -1,13 +1,28 @@
-#An execution flow in a programe in the order for wich instruction are in
+#An execution flow in a programe in the order for which instruction are in
 #Conditionals work as intruction flow manager 
 #A conditional contain a block of code that will check if some condition happend to choose wich one run or not, to continue executing
 print("Programa de evaluación de Notas:")
-notaAlumno=input("Introduce tu nota: ")#This variable will receive a value by console, just intput will be consider as a String
-def evaluar(nota):#An example using a function that check if the condition happend and depend the note the value of the value returned will be different
+notaAlumno=int(input("Introduce tu nota: "))#This variable will receive a value by console, just intput will be consider as a String
+def evaluar(nota):#An example using a function that check if the cndition happend and depend the note the value of the value returned will be different
     valoracion="aprobado"
     if nota <5:
         valoracion="suspenso"
     return valoracion
+print(evaluar(notaAlumno))
 #variable nota is only able to handle in his ambito, in his block or function therefore that variable only make sense in his ambito
 
     # print(evaluar(int(notaAlumno)))#Class integer to trasform the input into str to int
+
+edad=int(input("Cual es tu edad:"))
+def checkEdad(age):
+    mensaje="La edad"
+    if 18>age:
+        print("No puedes pasar")
+    elif age>100:
+        print("La edad no es valida")
+    else:
+        print("puedes pasar")
+    
+checkEdad(edad)
+
+
